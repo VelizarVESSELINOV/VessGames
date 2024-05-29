d021f041277b7d85ef7930953f69e4b5dcd174d7
BabyDuckSquad <BabyDuckSquad@gmail.com>
2024-05-29 15:04:30 -0500

    Added argument, percenteges added insted of scores,

--- quiz_cmd.py
+++ quiz_cmd.py
@@ -1,57 +1,52 @@
 from argparse import ArgumentParser
-
+from typing import List, Dict, Any
 from colorama import Fore, Style
 from colorlog import DEBUG, INFO, ColoredFormatter, StreamHandler, getLogger
-
 from quiz_generator import quiz_sample
 
 
-def play_capital_quiz(questions=5, answers=4, option="country"):
+def play_capital_quiz(questions: int = 5, answers: int = 4, option: str = "country") -> None:
     """Play US Capitals quiz."""
-    questions = quiz_sample(questions, answers, option)
+    quiz_questions = quiz_sample(questions, answers, option)
     score = 0
 
-    for i, num in enumerate(questions):
-        question = f'{Fore.BLUE}{questions[i]["question"]}{Style.RESET_ALL}\n'
+    for i, question_data in enumerate(quiz_questions):
+        question_text = f'{Fore.BLUE}{question_data["question"]}{Style.RESET_ALL}\n'
+        
+        for j, answer in enumerate(question_data["options"]):
+            question_text += f"\t{chr(ord('a') + j).upper()}. {answer}\n"
 
-        for j, answer in enumerate(questions[i]["options"]):
-            question += f"\t{chr(ord("a") + j).upper()}. {answer}\n"
-
-        print(question)
-        possible_answers = "".join([chr(ord("a") + j).upper() for j in range(answers)])
+        print(question_text)
+        possible_answers = "".join([chr(ord('a') + j).upper() for j in range(answers)])
         possible_answers += " or " + possible_answers.lower()
 
         while True:
             try:
-                answer = input(f"Answer [{possible_answers}]: ")
-                # print(f"answer: {answer}")
-                answer = questions[i]["options"][ord(answer[0].lower()) - ord("a")]
-            except IndexError:
-                print(
-                    f"Expected answer {Style.BRIGHT}[{possible_answers}]{Style.RESET_ALL}"
-                )
+                answer = input(f"Answer [{possible_answers}]: ").strip().lower()
+                if len(answer) != 1 or answer not in possible_answers.lower():
+                    raise ValueError
+                selected_answer = question_data["options"][ord(answer) - ord('a')]
+            except (IndexError, ValueError):
+                print(f"Expected answer {Style.BRIGHT}[{possible_answers}]{Style.RESET_ALL}")
             else:
                 break
 
-        correct_answer = questions[i]["answer"]
+        correct_answer = question_data["answer"]
 
-        if answer == correct_answer:
+        if selected_answer == correct_answer:
             score += 1
-            print(
-                f"\t{Fore.GREEN + Style.BRIGHT}Good answer, points: {score}/{i + 1}{Style.RESET_ALL}"
-            )
+            percentage = score / (i + 1) * 100
+            print(f"\t{Fore.BLUE + Style.BRIGHT}Good answer, percentage: {percentage}%{Style.RESET_ALL}")
         else:
-            print(
-                f"\t{Fore.RED + Style.BRIGHT}Bad answer, the correct answer is {correct_answer}, points: {score}/{i + 1}{Style.RESET_ALL}"
-            )
+            print(f"\t{Fore.YELLOW + Style.BRIGHT}Bad answer, the correct answer is {correct_answer}, percentage: {percentage}%{Style.RESET_ALL}")
 
-    print(f"{Style.BRIGHT}Final score: {score}/{questions}{Style.RESET_ALL}")
+    print(f"{Style.BRIGHT}Final score: {score}/{questions * 100}{Style.RESET_ALL}")
 
 
-def configure_logging(verbose):
-    # Define the color formatter
+def configure_logging(verbose: bool) -> None:
+    """Configure logging with color support."""
     color_formatter = ColoredFormatter(
-        "%(log_color)s%(levelname)s: " "%(name)s: " "%(message)s",
+        "%(log_color)s%(levelname)s: %(name)s: %(message)s",
         log_colors={
             "DEBUG": "cyan",
             "INFO": "green",
@@ -61,37 +56,26 @@ def configure_logging(verbose):
         },
     )
 
-    # Get the root logger
     logger = getLogger()
+    logger.setLevel(DEBUG if verbose else INFO)
 
-    # Set the logging level based on the verbose flag
-    if verbose:
-        logger.setLevel(DEBUG)
-    else:
-        logger.setLevel(INFO)
-
-    # Create a stream handler
     stream_handler = StreamHandler()
     stream_handler.setFormatter(color_formatter)
-
-    # Add the handler to the logger
     logger.addHandler(stream_handler)
 
 
-def main():
-    parser = ArgumentParser(description="Example script with verbose logging")
-    parser.add_argument(
-        "-v", "--verbose", action="store_true", help="Enable verbose logging"
-    )
+def main() -> None:
+    """Main entry point for the script."""
+    parser = ArgumentParser(description="Quiz game with verbose logging")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose logging")
+    parser.add_argument("-q", "--questions", type=int, default=5, help="Number of questions")
+    parser.add_argument("-a", "--answers", type=int, default=4, help="Number of answer options")
+    parser.add_argument("-o", "--option", type=str, default="country", help="Quiz option (e.g., country, us_state)")
 
-    # Parse command line arguments
     args = parser.parse_args()
 
-    # Configure logging
     configure_logging(args.verbose)
-
-    play_capital_quiz(questions=8)
-    play_capital_quiz(option="us_state")
+    play_capital_quiz(questions=args.questions, answers=args.answers, option=args.option)
 
 
 if __name__ == "__main__":
