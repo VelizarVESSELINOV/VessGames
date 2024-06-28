43f63f3bed82c3933796f111af28f2a7da33849b
VV <velizar.vesselinov@gmail.com>
2024-06-28 16:01:43 -0500

    animation

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,34 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    }
-  }
+  content: [
+    './pages/**/*.{html,js}',
+    './templates/*.{html,js}',
+    './components/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {
+      'animation': {
+            'text':'text 5s ease infinite',
+        },
+        'keyframes': {
+            'text': {
+                '0%, 100%': {
+                   'background-size':'200% 200%',
+                    'background-position': 'left center'
+                },
+                '50%': {
+                   'background-size':'200% 200%',
+                    'background-position': 'right center'
+                }
+            },
+        }
+    },
+  },
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "selector",
+  plugins: [
+    require("@tailwindcss/forms"),
+    tailwindcss: {},
+    autoprefixer: {},
+  ],
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  darkMode: "selector",
-  plugins: [
-    require("@tailwindcss/forms"),
-    // ...
+  content: [
+    "./src/**/*.{html,js}",
+    "./templates/*.{html,js}",
+    "./components/**/*.{html,js}",
   ],
+  theme: {
+    extend: {
+      animation: {
+        text: "text 5s ease infinite",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+    },
+  },
+  darkMode: "selector",
+  plugins: [require("@tailwindcss/forms")],
 };
